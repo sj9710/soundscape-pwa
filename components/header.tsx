a6ce1f8be8326288229ff623c1a0160cf24393f2
Roy <roy@pop-os.localdomain>
2023-01-09 21:02:11 +0530

    adding appbar and dark theme

---
+++ components/header.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+const Header = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const appBar: any = document.querySelector(".app-bar");
+      if (window.scrollY > 0) {
+        appBar.classList.add("scrolled");
+      } else {
+        appBar.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" className="app-bar">
+        <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Button color="inherit">Siddh</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+export default Header;
--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-})
+// /** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+// })
 
-module.exports = withPWA({
-  // config
-})
+// module.exports = withPWA({
+//   // config
+// })
+const withPWA = require('next-pwa');
+
+const settings = {
+  env: {
+  },
+  devIndicators: {
+    autoPrerender: false,
+  },
+  pwa: {
+    dest: 'public',
+  },
+};
+
+module.exports = process.env.NODE_ENV === 'development' ? settings : withPWA(settings);
